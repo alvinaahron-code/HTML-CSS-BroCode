1e945046d6dcbff8f46c67512def688d54386f08
Aahron <alvinaahron.code@gmail.com>
2025-10-11 08:33:14 +0800

    Car Example

---
+++ ObjectOrientedProgramming/Car/Car.java
@@ -0,0 +1,17 @@
+package Car;
+
+public class Car {
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "Blue";
+    double price = 50000.00;
+
+    void drive() {
+        System.out.println("You started to drive...");
+    }
+
+    void brake() {
+        System.out.println("You stepped on the brakes...");
+    }
+}
