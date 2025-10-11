f0a9d92366474a7463e8d3c048e4b706837f87df
Aahron <alvinaahron.code@gmail.com>
2025-10-11 18:43:36 +0800

    Inheritance

---
+++ ObjectOrientedProgramming/Inheritance/Dog.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Dog extends Animal {
+
+    double weight = 75.0;
+
+    void speak() {
+        System.out.println("The dog goes *woof*");
+    }
+    
+}
---
+++ ObjectOrientedProgramming/Inheritance/Sheep.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Sheep extends Animal {
+
+    double weight = 80.0;
+
+    void speak() {
+        System.out.println("The sheep goes *meeh*");
+    }
+    
+}
