11ef2c2d79321845c444fe9bd373c996c21ab8f6
Aahron <alvinaahron.code@gmail.com>
2025-10-11 09:05:08 +0800

    Human.java added

--- ObjectOrientedProgramming/Human/Main.java
+++ ObjectOrientedProgramming/Human/Main.java
@@ -6,8 +6,18 @@ public class Main {
         // Constructor = Special method that is called when an object is instatiated (created)
         // Check Human.java to see how to create the constructor
         // To create a human we need to send our constructor the parameters set
-        Human human = new Human("Aahron", 19, 55); 
-        
+
+        System.out.println("=============================================");
+        Human human1 = new Human("Rick", 50, 65); 
+        Human human2 = new Human("Morty", 18, 55);
+
+        System.out.println("Name: " + human1.name + " | Age: " + human1.age + " | Weight: " + human1.weight);
+        System.out.println("Name: " + human2.name + " | Age: " + human2.age + " | Weight: " + human2.weight);
+
+        System.out.println("---------------------------------------------");
+
+        human1.eat();
+        human2.drink();
 
     }
 }
