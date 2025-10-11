7c76b5b1e40e8f07f9365f64d46455df12b2da33
Aahron <alvinaahron.code@gmail.com>
2025-10-11 19:22:22 +0800

    super keyword

--- ObjectOrientedProgramming/Super/Student.java
+++ ObjectOrientedProgramming/Super/Student.java
@@ -2,6 +2,9 @@ package Super;
 
 public class Student extends Person{
     
+    // We used super (firstName, lastName) because we extended Person class
+    // So instead of this.firstName = firstName;
+    // Since it was already initialized in Person class we used super
     double GWA;
 
     Student(String firstName, String lastName, double GWA) {
