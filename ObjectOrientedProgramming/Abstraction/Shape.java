9ed34fcfcdfe095dd56f3fb9d056ccfeb57fbc03
Aahron <alvinaahron.code@gmail.com>
2025-10-12 10:33:06 +0800

    Abstraction

---
+++ ObjectOrientedProgramming/Abstraction/Shape.java
@@ -0,0 +1,11 @@
+package Abstraction;
+
+public abstract class Shape {
+
+    abstract double area(); //ABSTRACT
+
+    void display() { //CONCRETE
+        System.out.println("This is a shape");
+    }
+    
+}
